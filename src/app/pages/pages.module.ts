fced3f7f0ce315fec0e846a9290362002d4f6bcb
Wilknis Deyvis <wilknisoliveira@gmail.com>
2024-08-12 22:37:34 -0300

    delete: removing unexist components from page module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RoutingModule } from './routing.modules';
 import { AuthComponent } from './auth/auth.component';
 import { MatIconModule } from '@angular/material/icon';
-import { EmailInputComponent } from '../shared/email-input/email-input.component';
-import { PasswordInputComponent } from '../shared/password-input/password-input.component';
-import { ButtonFlatComponent } from '../shared/button-flat/button-flat.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -18,9 +15,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
         CommonModule,
         RoutingModule,
         MatIconModule,
-        EmailInputComponent,
-        PasswordInputComponent,
-        ButtonFlatComponent,
         MatButtonModule,
         MatDividerModule,
         FormsModule,
