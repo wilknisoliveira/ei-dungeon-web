e84102af953fd0889576dad2c9fd11a00f1e0b75
Wilknis Deyvis <wilknisoliveira@gmail.com>
2024-08-16 23:29:17 -0300

    feat: snackbar

--- src/app/guards/has-role-guards.guard.ts
+++ src/app/guards/has-role-guards.guard.ts
@@ -1,10 +1,12 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../service/auth/auth.service';
 import { inject } from '@angular/core';
+import { SnackbarService } from '../service/snackbar/snackbar.service';
 
 export const HasRoleGuardsGuard: CanActivateFn = (route, state) => {
     const router: Router = inject(Router);
     const authService: AuthService = inject(AuthService);
+    const snackBar: SnackbarService = inject(SnackbarService);
 
     const rolesRoute = route.data['roles'] as string[];
     const userRoles = authService.getRoles();
@@ -13,6 +15,7 @@ export const HasRoleGuardsGuard: CanActivateFn = (route, state) => {
 
     if (!isAuthorized) {
         console.error("You don't has access to this route.");
+        snackBar.addError("You don't has access to this route.");
         router.navigate(['home']);
     }
 
---
+++ src/app/service/snackbar/snackbar.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable()
+export class SnackbarService {
+    constructor(private snackBar: MatSnackBar) {}
+
+    addSuccess(msg: string) {
+        this.snackBar.open(msg, '', {
+            duration: 5 * 1000,
+            panelClass: '.snackbar-sucess',
+        });
+    }
+
+    addError(msg: string) {
+        this.snackBar.open(msg, '', {
+            duration: 5 * 1000,
+            panelClass: '.snackbar-error',
+        });
+    }
+}
