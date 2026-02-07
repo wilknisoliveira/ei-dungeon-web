f3d3ed89e80a9e104363f49375eb99b3a1a55739
Wilknis <wilknisoliveira@gmail.com>
2026-02-06 21:20:13 -0300

    style(home): theme mode with icons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -129,4 +129,8 @@ export class HomeComponent implements OnInit {
             }
         });
     }
+
+    toggleTheme(): void {
+        this.switchTheme.setValue(!this.switchTheme.value);
+    }
 }
