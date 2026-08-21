b12177e7f38bac96188fef9d4a4acecd3182be1d
Wilknis <wilknisoliveira@gmail.com>
2026-08-21 15:51:48 -0300

    feat: using material tokens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { httpInterceptorProviders } from './http-interceptors';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './http-interceptors/auth-interceptor';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
@@ -13,11 +13,10 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        HttpClientModule,
         MatSnackBarModule,
         AppComponent,
     ],
-    providers: [httpInterceptorProviders],
+    providers: [provideHttpClient(withInterceptors([authInterceptor]))],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
